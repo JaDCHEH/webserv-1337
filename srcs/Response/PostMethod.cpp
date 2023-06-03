10cf840f1e165766d79af2123770de71c6486b7c
Jad Chehboune <cjad@e1r9p8.1337.ma>
2023-06-03 13:55:26 +0100

    update on post

--- srcs/Response/PostMethod.cpp
+++ srcs/Response/PostMethod.cpp
@@ -46,24 +46,14 @@ int	Response::Post_method(Request &Request)
 {
 	if (Request._location.get_element("upload") == "on")
 	{
-		if (Request.getHeader("File-Name") != "")
-		{
-			std::ofstream file(Request._location.get_element("upload_dir")
-				+ Request.getHeader("File_Name") + get_extention(Request.getHeader("Content-Type")));
-			if (!file)
-				return simple_Response(Request, "404");
-			file << Request.body;
-		}
-		else
-		{
-			std::srand(time(NULL));
-			string name = std::to_string(rand() / 5);
-			std::ofstream file(Request._location.get_element("upload_dir")
-				+ name + get_extention(Request.getHeader("Content-Type")));
-			if (!file)
-				return simple_Response(Request, "404");
-			file << Request.body;
-		}
+		std::srand(time(NULL));
+		string name = std::to_string(rand() / 5);
+		std::ofstream file(Request._location.get_element("upload_dir")
+			+ name + get_extention(Request.getHeader("Content-Type")));
+		if (!file)
+			return simple_Response(Request, "404");
+		file << Request.body;
+		file.close();
 		return simple_Response(Request, "201");
 	}
 	else // CGI part // I need to check if file or dir 
